91efb6570caf00753757db3e11d4d78df867c423
Viana Ferguson <vianaferguson@yahoo.com>
2023-12-10 11:21:04 -0700

    first commit

---
+++ .history/assets/js/submit_20230529112632.js
@@ -0,0 +1,17 @@
+const submitContactForm = ()=>{
+    Email.send({
+      Host : "smtp.gmail.com",
+      Port : "465",
+      Username : "colormehighco@gmail.com",
+      Password : "ximhzscrxucoyvrh",
+      To : 'djveev86@gmail.com',
+      From : document.getElementById("email").value,
+      Subject : document.getElementById("subject").value,
+      Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
+  }
+
+
+//   html
